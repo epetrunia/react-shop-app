64a18f826538b91198ee2db871ab14a6c8cad70d
Eugenia Petrunia <avepetrunia@gmail.com>
2020-11-03 21:34:32 +0200

    Add styling for menu

--- src/components/MenuItem/index.jsx
+++ src/components/MenuItem/index.jsx
@@ -3,17 +3,19 @@ import styles from './MenuItem.module.scss';
 import classNames from 'classnames';
 
 function MenuItem({ title, imageUrl, large }) {
-  const backgroundClass = classNames(styles.menuItem, {
+  const backgroundStyle = classNames(styles.menuItem, {
     [styles.large]: large,
   });
   return (
-    <div
-      className={backgroundClass}
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    >
-      <div className={styles.content}>
-        <h1 className={styles.title}>{title}</h1>
-        <span className={styles.subtitle}>Shop Now</span>
+    <div className={backgroundStyle}>
+      <div
+        className={styles.backgroundImage}
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      >
+        <div className={styles.content}>
+          <h1 className={styles.title}>{title.toUpperCase()}</h1>
+          <span className={styles.subtitle}>Shop Now</span>
+        </div>
       </div>
     </div>
   );

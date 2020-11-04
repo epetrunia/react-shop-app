4d02a42b7e66d3ab268ff422cb71ff3adc18739c
Eugenia Petrunia <avepetrunia@gmail.com>
2020-11-04 15:46:43 +0200

    Add MenuItems to navigate withRouter HOC

--- src/components/Directory/index.jsx
+++ src/components/Directory/index.jsx
@@ -12,28 +12,33 @@ class Directory extends Component {
           title: 'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
           id: 1,
+          linkUrl: 'hats',
         },
         {
           title: 'jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
           id: 2,
+          linkUrl: '',
         },
         {
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
           id: 3,
+          linkUrl: '',
         },
         {
           title: 'womens',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           large: true,
           id: 4,
+          linkUrl: '',
         },
         {
           title: 'mens',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           large: true,
           id: 5,
+          linkUrl: '',
         },
       ],
     };
@@ -42,8 +47,8 @@ class Directory extends Component {
     const { sections } = this.state;
     return (
       <div className={styles.directoryMenu}>
-        {sections.map(({ title, imageUrl, large, id }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} large={large} />
+        {sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );

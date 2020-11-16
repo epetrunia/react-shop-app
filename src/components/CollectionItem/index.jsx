e9bc0db96ee5ad1541d81ac07ce3710d581a6cd4
Eugenia Petrunia <avepetrunia@gmail.com>
2020-11-16 12:42:14 +0200

    Add additional styling for CollectionItem

--- src/components/CollectionItem/index.jsx
+++ src/components/CollectionItem/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './CollectionItem.module.scss';
 
+import CustomButton from './../CustomButton';
+
 function CollectionItem({ name, price, imageUrl }) {
   return (
     <div className={styles.container}>
@@ -11,6 +13,9 @@ function CollectionItem({ name, price, imageUrl }) {
       <div className={styles.footer}>
         <span className={styles.name}>{name}</span>
         <span className={styles.price}>{price}$</span>
+      </div>
+      <div className={styles.btn}>
+        <CustomButton inverted>add to cart</CustomButton>
       </div>
     </div>
   );

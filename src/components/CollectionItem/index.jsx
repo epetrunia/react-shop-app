e03617a32ab9411740f1a621ed9292d0bdf647c2
Eugenia Petrunia <avepetrunia@gmail.com>
2020-11-16 13:34:58 +0200

    Add adding items to cart feature

--- src/components/CollectionItem/index.jsx
+++ src/components/CollectionItem/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import styles from './CollectionItem.module.scss';
 
 import CustomButton from './../CustomButton';
+import { addItem } from '../../redux/cart/actions';
 
-function CollectionItem({ name, price, imageUrl }) {
+function CollectionItem({ item, addItem }) {
+  const { name, price, imageUrl } = item;
   return (
     <div className={styles.container}>
       <div
@@ -15,10 +18,16 @@ function CollectionItem({ name, price, imageUrl }) {
         <span className={styles.price}>{price}$</span>
       </div>
       <div className={styles.btn}>
-        <CustomButton inverted>add to cart</CustomButton>
+        <CustomButton inverted onClick={() => addItem(item)}>
+          add to cart
+        </CustomButton>
       </div>
     </div>
   );
 }
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);

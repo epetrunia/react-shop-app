3f50290435541aaeeb1616b9d5d44a0a5a14c1ea
Eugenia Petrunia <avepetrunia@gmail.com>
2020-11-27 21:57:10 +0200

    Add ShopData to app

--- src/pages/Shop/index.jsx
+++ src/pages/Shop/index.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
 import CollectionOverview from './../../components/CollectionOverview';
 import Collection from '../Collection';
 
-function Shop({ match }) {
-  return (
-    <div>
-      <Route exact path={`${match.path}`} component={CollectionOverview} />
-      <Route path={`${match.path}/:collectionId`} component={Collection} />
-    </div>
-  );
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from './../../firebase/utils';
+
+class Shop extends Component {
+  unsubscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const collectionRef = firestore.collection('collections');
+    collectionRef.onSnapshot(async (snapshot) =>
+      convertCollectionsSnapshotToMap(snapshot)
+    );
+  }
+
+  render() {
+    const { match } = this.props;
+    return (
+      <div>
+        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route path={`${match.path}/:collectionId`} component={Collection} />
+      </div>
+    );
+  }
 }
 
 export default Shop;

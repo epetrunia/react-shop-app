523406e87214a138ac04c162a0b9f1564be95ac5
Eugenia Petrunia <avepetrunia@gmail.com>
2020-11-27 18:42:29 +0200

    Move Shop data to Firebase

--- src/firebase/utils.js
+++ src/firebase/utils.js
@@ -38,10 +38,24 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 };
 
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach((obj) => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+  return await batch.commit();
+};
+
 firebase.initializeApp(config);
 
-export const firestore = firebase.firestore();
 export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });

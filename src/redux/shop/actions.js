cd6daee321dc41646fdf66dc7971e9f488e57cf5
Eugenia Petrunia <avepetrunia@gmail.com>
2020-12-01 17:23:40 +0200

    Move collection fetch from Thunk into Saga

--- src/redux/shop/actions.js
+++ src/redux/shop/actions.js
@@ -18,18 +18,3 @@ export const fetchCollectionsFailure = (errorMessage) => ({
   type: shopActionTypes.FETCH_COLLECTIONS_FAILURE,
   payload: errorMessage,
 });
-
-export const fetchCollectionsAsync = () => {
-  return (dispatch) => {
-    const collectionRef = firestore.collection('collection');
-    dispatch(fetchCollectionsStart());
-
-    collectionRef
-      .get()
-      .then((snapshot) => {
-        const collectionMap = convertCollectionsSnapshotToMap(snapshot);
-        dispatch(fetchCollectionsSuccess(collectionMap));
-      })
-      .catch((error) => dispatch(fetchCollectionsFailure(error.message)));
-  };
-};
--- src/redux/shop/saga.js
+++ src/redux/shop/saga.js
@@ -1,8 +1,21 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, call, put } from 'redux-saga/effects';
+
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from './../../firebase/utils';
+import { fetchCollectionsFailure, fetchCollectionsSuccess } from './actions';
 import shopActionTypes from './types';
 
 export function* fetchCollectionsAsync() {
-  yield console.log('get fired');
+  try {
+    const collectionRef = firestore.collection('collection');
+    const snapshot = yield collectionRef.get();
+    const collectionMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+    yield put(fetchCollectionsSuccess(collectionMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
 }
 
 export function* fetchCollectionsStart() {

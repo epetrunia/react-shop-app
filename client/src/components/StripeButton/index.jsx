d9987fba2a02f0e684e510a808e455d1a8e1d5a7
Eugenia Petrunia <avepetrunia@gmail.com>
2020-12-09 01:04:56 +0200

    Connect client to server

--- client/src/components/StripeButton/index.jsx
+++ client/src/components/StripeButton/index.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 function StripeButton({ price }) {
   const priceForStripe = price * 100;
   const publishableKey =
     'pk_test_51Hr0BBHHs9Gl9GPhrM3btiAxkqm6RwYmotYYqcHHSjT9YEU3FFS2XrPVincqkYNTBKpNTk9op5P9jZcq9aZ57jIH008P1xHNHO';
+
   const onToken = (token) => {
-    console.log(token);
-    alert('Successful payment');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert('Payment was successful');
+      })
+      .catch((error) => {
+        console.log('Payment error ', JSON.parse(error));
+        alert('There was an issue with your payment');
+      });
   };
+
   return (
     <StripeCheckout
       label='Pay Now'
